7b16b96ab723060e0fb6cc3d0248551cb9104cd6
arif <9104011907000004>
2023-07-16 16:29:57 +0700

    create delete user function & isOwner middleware

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUsers } from '../model/users'
+import { deleteUserById, getUsers } from '../model/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -7,6 +7,18 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
         return res.status(200).json(users)
     } catch (error) {
         console.log();
+        return res.sendStatus(400)
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params
+        const deletedUser = await deleteUserById(id)
+        return res.json(deletedUser)
+        
+    } catch (error) {
+        console.log(error);
         return res.sendStatus(400)
     }
 }
