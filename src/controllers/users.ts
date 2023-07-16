859fdcabdf4557e878b9b220f27f3345ba3360f2
arif <9104011907000004>
2023-07-16 16:38:29 +0700

    create update user function

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUserById, getUsers } from '../model/users'
+import { deleteUserById, getUserById, getUsers } from '../model/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -17,6 +17,24 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const deletedUser = await deleteUserById(id)
         return res.json(deletedUser)
         
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400)
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params
+        const { username } = req.body
+        if (!username) res.sendStatus(400)
+
+        const user = await getUserById(id)
+        user.username = username
+        user.save()
+
+        return res.status(200).json(user).end()
+
     } catch (error) {
         console.log(error);
         return res.sendStatus(400)
