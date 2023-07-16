36f9480397a920334574309acf65af627076d03f
arif <9104011907000004>
2023-07-16 15:42:57 +0700

    create login function

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -39,3 +39,47 @@ export const register = async (req: express.Request, res: express.Response) => {
         
     }
 }
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+
+        const { email, password } = req.body
+
+        // validation
+        if (!email || !password) return res.sendStatus(400)
+
+        // get user by email
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+
+        // if user no found
+        if (!user) return res.sendStatus(400)
+
+        // get user hashed password
+        const expectedHash = authentication(user.authentication.salt, password)
+
+        // check if password is correct
+        if (user.authentication.password != expectedHash) {
+            return res.sendStatus(403)
+        }
+
+        const salt = random()
+
+        // create session token with user _id
+        user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+        await user.save()
+
+        // pass the session token into cookie
+        res.cookie('AUTH-SESSION', user.authentication.sessionToken, {
+            domain: 'localhost',
+            path: '/'
+        })
+
+        return res.status(200).json(user).end()
+        
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400)
+        
+    }
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 
 // get auth secret from env
-const secret = process.env.AUTH_SECRE
+const secret = process.env.AUTH_SECRET
 
 export const random = () => crypto.randomBytes(128).toString('base64')
 export const authentication = (salt: string, password: string) => {
